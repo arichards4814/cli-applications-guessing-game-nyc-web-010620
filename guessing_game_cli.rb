53fb93b10d6aa3de31c324a954e9312d5f8a2d66
Andrew Richards <arichards4814@gmail.com>
2019-12-09 23:18:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,12 @@
 # Code your solution here!
 
-def run_guessing_game ()
-  number = rand(6)
+def run_guessing_game 
+  number = rand(6) + 1
   answer = gets.chomp
   
-  if answer == number do
+  if answer == number 
+
+
   
   elsif answer == "exit"
   puts "Goodbye!"
